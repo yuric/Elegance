74c0805145dc133a04350d9952df9e937cb8ca5c
Yuri Costa <yuri@sparkroad.com>
2013-10-28 22:49:35 -0700

    added poll model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Elegance::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'polls#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131029053924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "polls", force: true do |t|
+    t.integer  "voter_id"
+    t.text     "question"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
