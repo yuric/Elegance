a71972baecedbc9e67dd5108d933a80dede15243
Yuri Costa <yuri@sparkroad.com>
2013-10-29 23:41:49 -0700

    passing tests

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,5 +1,6 @@
 require 'resolv'
 class Poll < ActiveRecord::Base
+  has_many :answers
   validates_presence_of :question
   validates :ip, :presence => true, :format => { :with => Resolv::IPv4::Regex }
   
