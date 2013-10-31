6e7d5ceae5ff59b21296d35cc93f0348244fe61d
Yuri Costa <yuri@sparkroad.com>
2013-10-30 21:10:08 -0700

    new tests for travis cl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elegance::Application.config.secret_key_base = 'b6d8232aa4b6e07a8f1723b3ebe26d1e2fbcb22a2bed484038dc5dd42b5e00b773b0367a5b100d87ace42937a7265a610ee4dbaf44df7f0b8df021d520e8b14b'
