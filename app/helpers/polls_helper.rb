256938e10f15ee83f327b8d0f335af339510a9f8
Yuri Costa <yuri@sparkroad.com>
2013-11-06 22:34:48 -0700

    votes

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,2 +1,16 @@
 module PollsHelper
+  def calculate_votes(poll)
+    votes = 0
+    poll.answers.each_with_index do |answer,i|
+      votes += answer.votes.count
+    end
+    if votes == 0
+      "0 votes"
+    elsif votes == 1
+      "1 vote"
+    else
+      "#{votes} votes"
+    end
+    
+  end
 end
